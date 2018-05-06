62777f9bf02cff07244b5b20f30118f49b829390
C <thefiercerabbit@gmail.com>
2018-05-06 20:05:36 +0100

    oi

--- js/init.js
+++ js/init.js
@@ -2,18 +2,18 @@ $(document).ready(function(){
     $('.button-collapse').sidenav();
 		$('.scrollspy').scrollSpy();
 		$('.parallax').parallax();
-		$('.head-link').click(function(e) {
-				e.preventDefault();
-				
-				var goto = $(this).attr('href');
-				
-				$('html, body').animate({
-						scrollTop: $(goto).offset().top-50
-				}, 800);
 
-		});
 });
 
+$('.head-link').click(function(e) {
+		e.preventDefault();
+		
+		var goto = $(this).attr('href');
+		
+		$('html, body').animate({
+				scrollTop: $(goto).offset().top-40
+		}, 800);		
+});
 
 function activate(page){
 		$("."+page).addClass("active");
