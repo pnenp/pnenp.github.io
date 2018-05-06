6ca03e6e455b6e38e23e973191ff6dd4d8a81b79
C <thefiercerabbit@gmail.com>
2018-05-06 20:15:30 +0100

    boi

--- js/init.js
+++ js/init.js
@@ -5,6 +5,16 @@ $(document).ready(function(){
 
 });
 
+$('.head-link').click(function(e) {
+		e.preventDefault();
+		
+		var goto = $(this).attr('href');
+		
+		$('html, body').animate({
+				scrollTop: $(goto).offset().top-40
+		}, 600);		
+});
+
 function activate(page){
 		$("."+page).addClass("active");
 }
