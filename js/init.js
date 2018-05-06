39e38de17b2d1b14e610990cc33d52b72af6880d
C <thefiercerabbit@gmail.com>
2018-05-06 01:03:00 +0100

    parallax and more

---
+++ js/init.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    $('.button-collapse').sideNav();
+});
+
+$('.parallax').parallax();
+$('.head-link').click(function(e) {
+    e.preventDefault();
+    
+    var goto = $(this).attr('href');
+
+    $('html, body').animate({
+	scrollTop: $(goto).offset().top -40
+    }, 800);
+
+});
+
+
+function activate(page){
+$("."+page).addClass("active");
+}
